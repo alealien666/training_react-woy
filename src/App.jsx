957e2ca3ca933f017a15189a6a3510f759417b7b
alealien666 <andraale28@gmail.com>
2024-10-10 10:45:02 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import './App.css'
+
+function App() {
+  return (
+    <div className="container mx-auto px-4">
+      <div className="flex justify-between">
+        <h1>ware ware wa ningen</h1>
+        <p>pitik</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
